1a041d0eeb31021eeb682cd82f15005242df18c1
oowliq <oowl.iq@gmail.com>
2020-06-02 08:23:11 +0300

    top-menu animation

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,5 +1,7 @@
 import React, { FC, useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import posed, { PoseGroup } from 'react-pose';
+import ClickOutHandler from 'react-onclickout';
 
 const TopMenuWrapper = styled.div`
     position: fixed;
@@ -24,7 +26,7 @@ const ShowButton = styled.button<{ open: boolean }>`
         content: '';
         height: 3px;
         right: 10px;
-        width: 10px;
+        width: ${(props) => (props.open ? '0' : '10px')};
         border-radius: 20em;
         background-color: #fff;
     }
@@ -35,7 +37,7 @@ const ShowButton = styled.button<{ open: boolean }>`
         content: '';
         left: 10px;
         height: 3px;
-        width: 10px;
+        width: ${(props) => (props.open ? '0' : '10px')};
         border-radius: 20em;
         background-color: #fff;
     }
@@ -48,6 +50,7 @@ const ShowButton = styled.button<{ open: boolean }>`
         left: 0;
         &::after {
             position: absolute;
+            transition: all 0.1s ease;
             top: 0;
             content: '';
             left: 10px;
@@ -56,33 +59,57 @@ const ShowButton = styled.button<{ open: boolean }>`
             width: 20px;
             border-radius: 20em;
             background-color: #fff;
+            ${(props) =>
+                props.open &&
+                css`
+                    transform: rotate(90deg);
+                `}
         }
     }
 
     &:hover {
         cursor: pointer;
-
-        &::before {
-            width: 20px;
-        }
-
-        &::after {
-            width: 20px;
-        }
     }
+
+    ${(props) =>
+        !props.open &&
+        css`
+            &:hover {
+                cursor: pointer;
+
+                &::before {
+                    width: 20px;
+                }
+
+                &::after {
+                    width: 20px;
+                }
+            }
+        `}
 `;
+
+const PosedTopMenuWrapper = posed(TopMenuWrapper)({
+    open: { width: '300px', height: '100px' },
+    closed: { width: '40px', height: '40px' },
+});
 
 const TopMenu: FC = () => {
     const [open, setOpen] = useState<boolean>(false);
 
     const handleToggle = (): void => setOpen((o) => !o);
 
+    const handleClose = (): void => {
+        if (open) setOpen(false);
+    };
+
     return (
-        <TopMenuWrapper>
-            <ShowButton open={open} onClick={handleToggle}>
-                <span />
-            </ShowButton>
-        </TopMenuWrapper>
+        <ClickOutHandler onClickOut={handleClose}>
+            <PosedTopMenuWrapper pose={open ? 'open' : 'closed'}>
+                <ShowButton open={open} onClick={handleToggle}>
+                    <span />
+                </ShowButton>
+            </PosedTopMenuWrapper>
+        </ClickOutHandler>
     );
 };
 
